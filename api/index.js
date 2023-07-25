b2eef8f623f7c31b9f715c8b2aebdec56ab4ba49
irechirech <irechirech@gmail.com>
2023-07-25 12:04:07 +0100

    modification

---
+++ api/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User.js');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const app = express();
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'zeoiudnz65dhj7jdh6jhd87dkj'
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+}));
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+});
+
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json(userDoc);
+            });
+        } else {
+            res.status(422).json('pass not ok');
+        }
+    } else {
+        res.json('not found');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    res.json({ token });
+});
+app.listen(4000, () => {
+    console.log('Server is listening on port 4000');
+});
